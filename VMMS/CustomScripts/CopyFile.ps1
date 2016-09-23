995f35106d998121af64198f4ec0afb69b081f01
Wasim <wasimbloch@gmail.com>
2016-09-23 22:25:52 +0530

    code

---
+++ VMMS/CustomScripts/CopyFile.ps1
@@ -0,0 +1,18 @@
+﻿<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
+param (
+	[string]$artifactsLocation,
+    [string]$folderName,
+    [string]$fileToInstall
+)
+Try
+{
+	$source = $artifactsLocation + "\$folderName\$fileToInstall"
+	$dest = "C:\WindowsAzure\$folderName\$fileToInstall"
+	Invoke-WebRequest $source -OutFile $dest
+	#Write-Output "SAS token is : $artifactsLocationSasToken"
+}
+Catch
+{
+	$ErrorMessage = $_.Exception.Message
+	exit 1
+}
---
+++ VMMS/CustomScripts/IsVmInMaint.ps1
@@ -0,0 +1,32 @@
+﻿# This sample uses the native powershell functionality for pulling dynamic metadata from a Windows Azure VM for incoming reboot events caused by 
+# planned maintenance. When reboot is pending, it will log an event in the EventLog.
+# Before executing it, the event needs to be registered in the EventLogger by executing: New-EventLog –LogName Application –Source “IsVmInMaint”
+# curl http://169.254.169.254/metadata/v1/maintenance
+# curl http://169.254.169.254/metadata/v1/InstanceInfo
+Try
+{
+	# Details of schedule task to execute.
+	# schtasks /create /tn myVmmsTask /tr "%SystemRoot%\System32\WindowsPowerShell\v1.0\Powershell.exe -NoProfile -ExecutionPolicy Bypass C:\Packages\Plugins\IsVmInMaint.ps1" /sc minute /mo 1 /ru System
+
+	New-EventLog –LogName Application –Source “IsVmInMaint”
+
+	# $result=curl http://169.254.169.254/metadata/latest/maintenance | findstr "^Content" | findstr -i "reboot"
+	$maintresult = Invoke-WebRequest -Uri ("http://169.254.169.254/metadata/latest/maintenance") -Method Get -UseBasicParsing
+	$result = $maintresult | findstr "^Content" | findstr -i "reboot"
+	if ($result) 
+	{
+		Write-EventLog -LogName Application –Source "IsVmInMaint" -EntryType Information –EventID 1 –Message "Incoming VM reboot"
+	}
+	else
+	{
+		Write-EventLog -LogName Application –Source "IsVmInMaint" -EntryType Information –EventID 1 –Message "No upcoming VM reboot"
+	}
+	$instanceresult = Invoke-WebRequest -Uri ("http://169.254.169.254/metadata/latest/InstanceInfo") -Method Get -UseBasicParsing
+	$result=$instanceresult | findstr "^Content"
+	if ($result) {Write-EventLog -LogName Application –Source "IsVmInMaint" -EntryType Information –EventID 1 –Message "VM Instance Info : $result"}
+}
+Catch
+{
+	$ErrorMessage = $_.Exception.Message
+	exit 1
+}
